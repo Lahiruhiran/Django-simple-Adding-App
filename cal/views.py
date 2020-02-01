1fcef805f5d28d579ec7b49cc44b320131e711ab
unknown <lahiruhiranwijesooriyansbm@gmail.com>
2020-02-01 22:02:08 +0530

    finalized the cal app

--- cal/views.py
+++ cal/views.py
@@ -6,4 +6,7 @@ def home(request):
     return render(request,'home.html',{'name':'Lahiru'})
 
 def add(request):
-    return render(request, 'result.html')
+    val1 = int(request.GET['no1'])
+    val2 = int(request.GET['no2'])
+    res = val1 + val2
+    return render(request, 'result.html',{'result':res})
