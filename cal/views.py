e9d58f4c6a923d3c9242bc55f32d0d826ebb21bb
unknown <lahiruhiranwijesooriyansbm@gmail.com>
2020-02-08 20:17:44 +0530

    Add the html page to the Django frameork

--- cal/views.py
+++ cal/views.py
@@ -6,7 +6,7 @@ def home(request):
     return render(request,'home.html',{'name':'Lahiru'})
 
 def add(request):
-    val1 = int(request.GET['no1'])
-    val2 = int(request.GET['no2'])
+    val1 = int(request.POST['no1'])
+    val2 = int(request.POST['no2'])
     res = val1 + val2
     return render(request, 'result.html',{'result':res})
